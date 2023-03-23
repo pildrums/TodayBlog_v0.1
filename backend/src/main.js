47f05e24ae21b174dbba14c8779a77b2b8dd4c59
pildrums <there7788@gmail.com>
2023-03-23 14:44:10 +0900

    Create FakeData - 페이지네이션, 글자수 제한, 보이는 개수 제한, 포스트 역순, 보이는 개수 제한을 위한 더미데이터

--- backend/src/main.js
+++ backend/src/main.js
@@ -4,6 +4,7 @@ import Router from "koa-router";
 import bodyParser from "koa-bodyparser";
 import mongoose from "mongoose";
 import api from "./api";
+import createFakeData from "./createFakeData";
 
 const { PORT, MONGO_URI } = process.env;
 mongoose.Promise = global.Promise;
@@ -12,6 +13,7 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
+    createFakeData();
   })
   .catch((e) => {
     console.error(e);
